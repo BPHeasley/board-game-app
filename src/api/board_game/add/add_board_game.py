8381f98d349ae5b79f491e7521ee464982d84785
Ben <bpheasley@gmail.com>
2024-09-13 15:07:22 -0500

    Adding API Gateway

--- src/api/board_game/add/add_board_game.py
+++ src/api/board_game/add/add_board_game.py
@@ -7,13 +7,14 @@ logger = Logger()
 
 dynamodb = boto3.resource('dynamodb')
 
+
 def add_board_game(event: dict):
     logger.info("Adding a new board game")
     board_game_detail = json.loads(event['body'])
     board_game_title = board_game_detail['title']
 
     ddb_item = {
-        'title':  board_game_title,
+        'title': board_game_title,
         'data': {
             'title': board_game_title,
         }
@@ -26,7 +27,8 @@ def add_board_game(event: dict):
 
     return board_game_detail
 
-def lambda_handler(event):
+
+def lambda_handler(event, context):
     try:
         board_game_detail = add_board_game(event=event)
         response = {
