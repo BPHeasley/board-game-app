c19425a6041813d1a3c152d71d98a304849713e2
Ben <bpheasley@gmail.com>
2024-09-17 11:38:14 -0500

    Adding get function

---
+++ src/api/board_game/get/get_board_game.py
@@ -0,0 +1,32 @@
+import boto3
+import json
+import os
+
+# from aws_lambda_powertools import Logger
+
+# logger = Logger()
+
+dynamodb = boto3.resource('dynamodb')
+board_games_table = os.getenv('TABLE_NAME')
+ddbTable = dynamodb.Table(board_games_table)
+
+
+def lambda_handler(event, context):
+    status_code = 400  # default response
+    try:
+        ddb_response = ddbTable.get_item(Key={'title': event['pathParameters']['title']})
+
+        if 'Item' in ddb_response:
+            response_body = ddb_response['Item']
+            status_code = 200
+        else:
+            response_body = {}
+    except Exception as err:
+        response_body = {'Error:': str(err)}
+        status_code = 400
+
+    return {
+        'statusCode': status_code,
+        'headers': {},
+        'body': json.dumps(response_body)
+    }
