1ebe3091917d0fffa72cc228a00edefb69e05810
Ben <bpheasley@gmail.com>
2024-09-19 11:52:28 -0500

    Adding unit tests

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,145 +1,134 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
 import json
-
+import os
+import boto3
+import uuid
 import pytest
+from moto import mock_dynamodb
+from contextlib import contextmanager
+from unittest.mock import patch
 
-from hello_world import app
-
-def lambda_context():
-    class LambdaContext:
-        def __init__(self):
-            self.function_name = "test-func"
-            self.memory_limit_in_mb = 128
-            self.invoked_function_arn = "arn:aws:lambda:eu-west-1:809313241234:function:test-func"
-            self.aws_request_id = "52fdfc07-2182-154f-163f-5f0f9a621d72"
-
-        def get_remaining_time_in_millis(self) -> int:
-            return 1000
-
-    return LambdaContext()
+MOCK_BOARD_GAME_TABLE_NAME = 'MockBoardGames'
+MOCK_BOARD_GAME_TITLE = 'unit-test-title'
+MOCK_BOARD_GAME_TITLE_2 = 'unit-test-title-2'
 
 
-@pytest.fixture()
-def apigw_event():
-    """ Generates API GW Event"""
-
-    return {
-   "body":"",
-   "headers":{
-      "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-      "Accept-Encoding":"gzip, deflate, br",
-      "Accept-Language":"pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
-      "Cache-Control":"max-age=0",
-      "Connection":"keep-alive",
-      "Host":"127.0.0.1:3000",
-      "Sec-Ch-Ua":"\"Google Chrome\";v=\"105\", \"Not)A;Brand\";v=\"8\", \"Chromium\";v=\"105\"",
-      "Sec-Ch-Ua-Mobile":"?0",
-      "Sec-Ch-Ua-Platform":"\"Linux\"",
-      "Sec-Fetch-Dest":"document",
-      "Sec-Fetch-Mode":"navigate",
-      "Sec-Fetch-Site":"none",
-      "Sec-Fetch-User":"?1",
-      "Upgrade-Insecure-Requests":"1",
-      "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
-      "X-Forwarded-Port":"3000",
-      "X-Forwarded-Proto":"http"
-   },
-   "httpMethod":"GET",
-   "isBase64Encoded":False,
-   "multiValueHeaders":{
-      "Accept":[
-         "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-      ],
-      "Accept-Encoding":[
-         "gzip, deflate, br"
-      ],
-      "Accept-Language":[
-         "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7"
-      ],
-      "Cache-Control":[
-         "max-age=0"
-      ],
-      "Connection":[
-         "keep-alive"
-      ],
-      "Host":[
-         "127.0.0.1:3000"
-      ],
-      "Sec-Ch-Ua":[
-         "\"Google Chrome\";v=\"105\", \"Not)A;Brand\";v=\"8\", \"Chromium\";v=\"105\""
-      ],
-      "Sec-Ch-Ua-Mobile":[
-         "?0"
-      ],
-      "Sec-Ch-Ua-Platform":[
-         "\"Linux\""
-      ],
-      "Sec-Fetch-Dest":[
-         "document"
-      ],
-      "Sec-Fetch-Mode":[
-         "navigate"
-      ],
-      "Sec-Fetch-Site":[
-         "none"
-      ],
-      "Sec-Fetch-User":[
-         "?1"
-      ],
-      "Upgrade-Insecure-Requests":[
-         "1"
-      ],
-      "User-Agent":[
-         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
-      ],
-      "X-Forwarded-Port":[
-         "3000"
-      ],
-      "X-Forwarded-Proto":[
-         "http"
-      ]
-   },
-   "multiValueQueryStringParameters":"",
-   "path":"/hello",
-   "pathParameters":"",
-   "queryStringParameters":"",
-   "requestContext":{
-      "accountId":"123456789012",
-      "apiId":"1234567890",
-      "domainName":"127.0.0.1:3000",
-      "extendedRequestId":"",
-      "httpMethod":"GET",
-      "identity":{
-         "accountId":"",
-         "apiKey":"",
-         "caller":"",
-         "cognitoAuthenticationProvider":"",
-         "cognitoAuthenticationType":"",
-         "cognitoIdentityPoolId":"",
-         "sourceIp":"127.0.0.1",
-         "user":"",
-         "userAgent":"Custom User Agent String",
-         "userArn":""
-      },
-      "path":"/hello",
-      "protocol":"HTTP/1.1",
-      "requestId":"a3590457-cac2-4f10-8fc9-e47114bf7c62",
-      "requestTime":"02/Feb/2023:11:45:26 +0000",
-      "requestTimeEpoch":1675338326,
-      "resourceId":"123456",
-      "resourcePath":"/hello",
-      "stage":"Prod"
-   },
-   "resource":"/hello",
-   "stageVariables":"",
-   "version":"1.0"
-}
+def mock_game():
+    return MOCK_BOARD_GAME_TITLE
 
 
-def test_lambda_handler(apigw_event):
+@contextmanager
+def my_test_environment():
+    with mock_dynamodb():
+        set_up_dynamodb()
+        put_data_dynamodb()
+        yield
 
-    ret = app.lambda_handler(apigw_event, lambda_context())
-    data = json.loads(ret["body"])
 
-    assert ret["statusCode"] == 200
-    assert "message" in ret["body"]
-    assert data["message"] == "hello world"
+def set_up_dynamodb():
+    conn = boto3.client(
+        'dynamodb'
+    )
+    conn.create_table(
+        TableName=MOCK_BOARD_GAME_TABLE_NAME,
+        KeySchema=[
+            {'AttributeName': 'title', 'KeyType': 'HASH'},
+        ],
+        AttributeDefinitions=[
+            {'AttributeName': 'title', 'AttributeType': 'S'}
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 1,
+            'WriteCapacityUnits': 1
+        }
+    )
+
+
+def put_data_dynamodb():
+    conn = boto3.client(
+        'dynamodb'
+    )
+    conn.put_item(
+        TableName=MOCK_BOARD_GAME_TABLE_NAME,
+        Item={
+            'title': {'S': MOCK_BOARD_GAME_TITLE}
+        }
+    )
+    conn.put_item(
+        TableName=MOCK_BOARD_GAME_TABLE_NAME,
+        Item={
+            'title': {'S': MOCK_BOARD_GAME_TITLE_2}
+        }
+    )
+
+
+@patch.dict(os.environ, {'TABLE_NAME': MOCK_BOARD_GAME_TABLE_NAME, 'AWS_XRAY_CONTEXT_MISSING': 'LOG_ERROR'})
+def test_get_list_of_board_games():
+    with my_test_environment():
+        from src.api.board_game import get
+        with open('./events/event-get-all-board-games.json', 'r') as f:
+            apigw_get_all_board_games_event = json.load(f)
+        expected_response = [
+            {
+                'title': MOCK_BOARD_GAME_TITLE,
+            },
+            {
+                'title': MOCK_BOARD_GAME_TITLE_2,
+            }
+        ]
+        ret = get.get_all_board_games.lambda_handler(apigw_get_all_board_games_event, '')
+        assert ret['statusCode'] == 200
+        data = json.loads(ret['body'])
+        assert data == expected_response
+
+
+def test_get_single_board_game():
+    with my_test_environment():
+        from src.api.board_game import get
+        with open('./events/event-get-board-game-by-title.json', 'r') as f:
+            apigw_event = json.load(f)
+        expected_response = {
+            'title': MOCK_BOARD_GAME_TITLE
+        }
+        ret = get.get_board_game.lambda_handler(apigw_event, '')
+        assert ret['statusCode'] == 200
+        data = json.loads(ret['body'])
+        assert data == expected_response
+
+
+def test_get_single_board_game_wrong_title():
+    with my_test_environment():
+        from src.api.board_game import get
+        with open('./events/event-get-board-game-by-title.json', 'r') as f:
+            apigw_event = json.load(f)
+        apigw_event['pathParameters']['title'] = 'wrong-title'
+        apigw_event['rawPath'] = '/boardgames/wrong-title'
+        ret = get.get_board_game.lambda_handler(apigw_event, '')
+        assert ret['statusCode'] == 404
+        assert json.loads(ret['body']) == {}
+
+
+# @patch('uuid.uuid1', mock_uuid)
+# @pytest.mark.freeze_time('2001-01-01')
+def test_add_board_game():
+    with my_test_environment():
+        from src.api.board_game import add
+        with open('./events/event-add-board-game.json', 'r') as f:
+            apigw_event = json.load(f)
+        expected_response = json.loads(apigw_event['body'])
+        ret = add.add_board_game.lambda_handler(apigw_event, '')
+        assert ret['statusCode'] == 200
+        data = json.loads(ret['body'])
+        assert data['title'] == MOCK_BOARD_GAME_TITLE
+
+
+# def test_delete_board_game():
+#     with my_test_environment():
+#         from src.api.board_game import delete
+#         with open('./events/event-delete-user-by-id.json', 'r') as f:
+#             apigw_event = json.load(f)
+#         ret = delete.delete_board_game.lambda_handler(apigw_event, '')
+#         assert ret['statusCode'] == 200
+#         assert json.loads(ret['body']) == {}
