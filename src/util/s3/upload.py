c264e9282c8ddcf4d4da6df1f7f564c1f9665a34
Ben <bpheasley@gmail.com>
2024-09-24 17:48:18 -0500

    Testing pipeline upload to s3 bucket

--- src/util/s3/upload.py
+++ src/util/s3/upload.py
@@ -1,17 +1,15 @@
 import os
-
 import boto3
 
-board_games_bucket = os.getenv('BOARD_GAME_BUCKET')
+board_games_bucket = os.getenv('BUCKET_NAME')
 
 
 def lambda_handler(event, context):
-    bucket_name = board_games_bucket
     file_name = 'index.html'
     file_content = 'This is the content of the file.'
 
     s3 = boto3.client('s3')
-    s3.put_object(Body=file_content, Bucket=bucket_name, Key=file_name)
+    s3.put_object(Body=file_content, Bucket=board_games_bucket, Key=file_name)
     return {
         'statusCode': 200,
         'body': 'File uploaded successfully.'
